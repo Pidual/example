6d9019e1e931b8d0a5d4d289015f6e6198b59c5a
Carlos <73614255+Pidual@users.noreply.github.com>
2024-04-06 19:07:34 -0500

    Gang plan k

--- src/main/java/org/distributed/citizens/CitizensApplication.java
+++ src/main/java/org/distributed/citizens/CitizensApplication.java
@@ -2,12 +2,19 @@ package org.distributed.citizens;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CitizensApplication {
+public class CitizensApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CitizensApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(CitizensApplication.class);
 	}
 
 }
